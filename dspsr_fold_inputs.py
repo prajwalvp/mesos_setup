26fc8315df8a6f08a1c60867999c1e442c51084b
ewanbarr <ewan.d.barr@gmail.com>
2018-07-11 20:27:08 +0200

    updated dspsr_fold

--- dspsr_fold_inputs.py
+++ dspsr_fold_inputs.py
@@ -7,7 +7,7 @@ import optparse
 
 def on_message(body,opts):
     folding_packet = json.loads(body.decode("utf=8"))
-    with open('%s.txt'%(candidate[:-3]),'w') as f:
+    with open('%s.txt'%(opts.output_file_path+folding_packet['source']),'w') as f:
         f.write('SOURCE: %s\n'%folding_packet['source'])
         f.write('PERIOD: %s\n'%folding_packet['period'])
         f.write('DM: %s\n'%folding_packet['dm'])
@@ -22,8 +22,8 @@ def on_message(body,opts):
     #for cand_file in glob.glob('*.txt'):    
     #subprocess.check_call(["dspsr","J1857+0943.fil","-c",str(period[0]),"-D",str(dm[0]),"-k","parkes","-b","128","-L","10","-A","-O","first_out"])
         #subprocess.check_call(["dspsr","-P",cand_file,path_file_name])
-    print "dspsr -P %s.txt %s" %(candidate[:-3],folding_packet['file path'])
-    subprocess.check_call(["dspsr","-P",cand_file,folding_packet['file path']])
+    print "dspsr -P %s.txt %s" %(folding_packet['source'],folding_packet['file path'])
+    subprocess.check_call(["dspsr","-P",opts.output_file_path+folding_packet['source']+".txt",folding_packet['file path'],"-O",opts.output_file_path+folding_packet['source']])
         #subprocess.check_call(["dspsr","-P",cand_file,'filterbank/J1857+0943.fil'])
 
 def main(opts):
